583903b375edaab53596a599025f2ff2e76a04ab
swadhinroutray <swadhin.routray@gmail.com>
2020-05-23 01:38:43 +0530

    Created Procedure for Feedback insertion, Fix: TODO:Line 25

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ const router  = require("express").Router();
 const auth = require(`./auth`);
 const user = require(`./user`);
 
+//Auth Routes
 router.get(`/`, auth.renderLogin);
 router.post('/login', auth.login);
 router.get(`/logout`,auth.logout)
 router.get('/register', auth.renderRegister);
 router.post('/registerForm', auth.register);
 
+//Booking Routes
 router.get(`/booking`, user.renderBooking);
 
+//Feedback Routes
+router.get(`/feedback`, user.renderFeedBack)
+router.post(`/feedbackForm`,user.feedback)
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,35 @@ exp.renderBooking = (req, res) => {
     res.render(`booking`, {});
 }
 
+exp.renderFeedBack = (req,res) =>{
+    res.render(`feedback`,{})
+}
+exp.feedback =  async (req,res) => {
+    let user_id = req.session.user_id;
+    let body   = String(req.body.feedback).trim();
+    let rating = req.body.rating;
+    [err,data] = await to(db.query(`CALL InsertFeedback(?, ?, ?);`, [user_id,body,rating]));
+    if(err) {
+        console.log(err)
+        return res.send('Error in Inserting Feedback');
+    } 
+
+    console.log("Inserted Feedback");
+    return res.send({success:true});
+    
+//Need to enable duplicate entry for feedback
+}
+
 module.exports = exp;
+
+/*
+Delimiter $$
+CREATE PROCEDURE `InsertFeedback`(
+in userId  int(11), 
+in Body  varchar(255), 
+in Rating   int(11))
+BEGIN
+	Insert into Feedback
+	Values(userId,Body,Rating);
+  END $$
+*/
